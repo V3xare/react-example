2ea5a8ebe35adea2a76df24f641fba764e0243c6
Vexare <calledhypocrisy@gmail.com>
2022-05-17 18:11:10 +0300

    start

---
+++ source/modules/app.tsx
@@ -0,0 +1,20 @@
+import React, { useRef, useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+import { Text } from "@components/Typography";
+import { Icons } from "@components/Icons";
+
+export const App = ( props: any ) => {
+
+	let [ counter, setCounter ] = useState( 44 );
+
+	return (
+		<div className="app">
+			<Text>
+				Hello World
+				<Icons.pencil/>
+			</Text>
+			<button onClick={() => setCounter( counter + 1 ) }>Click Me:{ counter }</button>
+		</div>
+	);
+};
